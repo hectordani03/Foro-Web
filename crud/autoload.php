ce10dafeb3163076bec7d40f05e3b566dcd79ff7
Esteban1414 <emancilla5@ucol.mx>
2024-02-26 12:49:18 -0600

    url

--- crud/autoload.php
+++ crud/autoload.php
@@ -1,13 +1,10 @@
 <?php
-    spl_autoload_register(function ($clase){
-    $archivo = __DIR__."/".$clase.".php";
+spl_autoload_register(function ($clase) {
+    $archivo = __DIR__ . "/" . $clase . ".php";
     $archivo = str_replace("\\", "/", $archivo);
-    
-    if(is_file($archivo)){
 
-            require_once $archivo;
+    if (is_file($archivo)) {
 
-        };
-    });
-
-?>
+        require_once $archivo;
+    };
+});
--- crud/index.php
+++ crud/index.php
@@ -1,23 +1,41 @@
-<?php 
+<?php
     require_once './config/app.php';
     require_once 'autoload.php';
     require_once './app/views/inc/session_start.php';
 
-    if(isset($_GET['views'])){
+    if (isset($_GET['views'])) {
 
         $url = explode("/", $_GET['views']);
-        
     } else {
         $url = ["login"];
     }
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-   <?php require_once "./app/inc/views/header.php"; ?>
+    <?php require_once "./app/views/inc/header.php"; ?>
 </head>
+
 <body>
 
-    <?php require_once "./app/inc/views/script.php"; ?>
+<?php
+
+    use app\controllers\viewsController;
+
+    $viewsController = new viewsController();
+    $views = $viewsController->getViewsController($url[0]);
+
+    if ($views == "login" || $views == "404") {
+
+        require_once "./app/views/content/" . $views . "-view.php";
+    } else {
+        require_once $views;
+        require_once "./app/views/inc/navbar.php";
+    }
+    require_once "./app/views/inc/script.php";
+
+?>
 </body>
+
 </html>
