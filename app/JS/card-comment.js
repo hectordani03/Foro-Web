4bffef6f68579f2650b47e8c28c3ddf5852476f1
hectordani03 <hectordaniel010203@gmail.com>
2024-03-08 11:49:11 -0600

    comment card

--- app/JS/card-comment.js
+++ app/JS/card-comment.js
@@ -1,4 +1,183 @@
 document.addEventListener("DOMContentLoaded", e => {
 
-    const commentBtn = document.getElementById('comment-button'); 
+    const commentBtn = document.getElementById('comment-button');
+    const $body = document.getElementById('body-content');
+    const $section = document.getElementById('section');
+    const capaComments = document.getElementById('capa-comments');
+
+    let modalVisible2 = false;
+
+    commentBtn.addEventListener('click', e => {
+        // Si el modal está visible, ocultarlo
+        if (modalVisible2) {
+            $section.style.display = 'none';
+            capaComments.classList.add("hidden");
+            modalVisible2 = false;
+        } else {
+            $section.innerHTML = `
+            <section class="bg-gray-100 rounded-xl w-7/12 mx-auto h-screen flex flex-col relative">
+            <div class="flex justify-between fixed w-7/12 z-50 shadow-inner bg-gray-100">
+            <h1 class="text-black flex text-2xl font-bold mt-5 ml-5">FOR <span class="text-blue-500 ml-2">US</span></h1>
+            <p class="font-bold text-xl mt-5">Publicacion de Jose Joshua</p>
+            <div id="close-button" class="text-gray-400 w-8 h-8 transition-all cursor-pointer mt-5 mr-5">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+            <path class="fill-current" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/>
+            </svg>
+            </div>
+            </div>
+            <hr class="w-full mt-5">
+            
+            <div id="card-scrollbar" class="overflow-y-auto z-40 h-auto">
+            <div class="bg-gray-100 w-10/12 rounded-xl flex flex-col relative mt-5 h-fit self-start opacity-100 mx-auto">
+            <div id="capa" class="bg-gray-500 opacity-30 w-full h-full absolute hidden rounded-xl transition ease-in"></div>
+            <div id="custom-modal" class="absolute w-full h-full flex flex-col justify-center items-center opacity-100 transition ease-in">
+            <!-- Contenido del modal -->
+            </div>
+            <!-- TOP CARD CONTENT -->
+            <div class="flex mt-5 ml-5">
+            <img class="w-12 h-11 bg-blue-500 rounded-full top-8 left-8" src="/app/assets/person-1.jpg" alt="">
+            <div class="flex flex-col ml-5">
+            <h2 class="text-xl text-gray-400 font-semibold">Jose Joshua</h2>
+            <p class="text-gray-400">08:04 p.m</p>
+            </div>
+            <!-- CARD MENU -->
+            <div class="absolute right-5 top-0">
+                            <span id="menu-card" class="text-5xl text-gray-400 cursor-pointer select-none">...</span>
+                            </div>
+                            </div>
+                            
+                            <!-- CARD CONTENT -->
+                            <p class="text-gray-400 w-10/12 mx-auto text-xl mt-8">"I firmly believe that climate action should be a global priority. Protecting our planet for future generations. It's time for all of us to commit to concrete and meaningful actions!"</p>
+    
+                            <!-- <a class="text-blue-500 underline text-xl w-10/12 mx-auto mt-2 mb-1" href="https://google.com">Naciones Unidas</a> -->
+                            <img class="w-10/12 mx-auto rounded-xl mt-7" src="/app/assets/card_img/obj2_img.jpg" alt="">
+                            
+                            <!-- CARD HASHTAGS -->
+                            <div class="flex gap-4 w-10/12 mx-auto mt-5">
+                        <a class="text-white font-bold bg-gray-400 rounded-full px-4 py-1 text-center" href="">#NoMoreHunger</a>
+                        <a class="text-white font-bold bg-gray-400 rounded-full px-4 py-1 text-center" href="">#EndIt</a>
+                        </div>
+                        
+                        <!-- FOOTER CARD -->
+                        <div class="flex w-10/12 relative mt-10 mb-5 mx-auto justify-between">
+                        <!-- BUTTONS FOOTER CARD -->
+                        <div id="like" class="text-gray-400 w-8 h-8 transition-all cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                <path class="fill-current" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+                            </svg>
+                        </div>
+                        <script>
+                        const like = document.getElementById('like')
+                        like.addEventListener('click', () => {
+                                like.classList.toggle('text-red-700')
+                            })
+                            </script>
+                            
+                            <div id="comment-button" class="text-gray-400 w-7 h-7 mt-1 cursor-pointer">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path class="fill-current" d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
+                            </svg>
+                        </div>
+    
+                        <div class="text-gray-400 w-8 h-8 cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="fill-current" d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"/></svg>
+                        </div>
+                    </div>
+                    </div>
+                    <hr class="w-full mt-5">
+                    
+                    <div class="w-10/12 mx-auto mb-20">
+                    <div class="flex gap-5 mt-10">
+                    <img class="w-16 h-11 bg-blue-500 rounded-full top-8 left-8" src="/app/assets/person-1.jpg" alt="">
+                    <div class="rounded-xl bg-gray-200 shadow-lg">
+                    <div class="ml-5 mr-5 mt-1 mb-2">
+                    <p class="text-gray-400 font-bold">Jose Joshua</p>
+                    <span class="text-gray-400">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nisi, cum consectetur,Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+                    </div>
+                    </div>
+                    </div>
+                    <!-- ICONS -->
+                    <div class="flex w-10/12 mx-auto mt-3 gap-5 relative">
+                    <div id="like2" class="text-gray-400 w-7 h-7 transition-all cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path class="fill-current" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+                    </svg>
+                    </div>
+                    <script>
+                    const like2 = document.getElementById('like2')
+                    like2.addEventListener('click', () => {
+                        like2.classList.toggle('text-red-700')
+                    })
+                    </script>
+                    <div id="comment-button" class="text-gray-400 w-6 h-6 mt-1 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="25" height="25" class="">
+                    <path class="fill-current" d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64h96v80c0 6.1 3.4 11.6 8.8 14.3s11.9 2.1 16.8-1.5L309.3 416H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64z"/>
+                    </svg>
+                    </div>
+                    <div class="text-gray-400 w-7 h-7 cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="fill-current" d="M307 34.8c-11.5 5.1-19 16.6-19 29.2v64H176C78.8 128 0 206.8 0 304C0 417.3 81.5 467.9 100.2 478.1c2.5 1.4 5.3 1.9 8.1 1.9c10.9 0 19.7-8.9 19.7-19.7c0-7.5-4.3-14.4-9.8-19.5C108.8 431.9 96 414.4 96 384c0-53 43-96 96-96h96v64c0 12.6 7.4 24.1 19 29.2s25 3 34.4-5.4l160-144c6.7-6.1 10.6-14.7 10.6-23.8s-3.8-17.7-10.6-23.8l-160-144c-9.4-8.5-22.9-10.6-34.4-5.4z"/></svg>
+                    </div>
+                    <div class="flex gap-3 absolute right-3">
+                            <div class="text-red-700 w-7 h-7 transition-all cursor-pointer mr-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                    <path class="fill-current" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+                                    </svg>
+                            </div>
+                            <span class="text-xl text-gray-400 font-semibold pb-2">8</span>
+                        </div>
+                        </div>
+                        </div>
+                        </div>
+                        
+                        <hr class="w-full mt-5">
+                        
+                        <section class="fixed bottom-0 bg-gray-100 w-7/12 shadow-inner z-50">
+                        <div class="w-11/12 mx-auto flex mt-5 mb-5">
+                        <img class="w-12 h-11 bg-blue-500 rounded-full top-8 left-8" src="/app/assets/person-1.jpg" alt="">
+                        <div class="bg-gray-200 ml-5 rounded-xl shadow-lg w-10/12">
+                        <!-- Reemplaza el párrafo con el input -->
+                        <!-- <input type="text" class="px-5 mt-2 text-gray-600 w-full focus:outline-none bg-gray-200" placeholder="Write a comment..." style="white-space: pre-wrap;"> -->
+                        <textarea class="relative rounded-lg text-lg text-gray-400 bg-gray-200 w-full resize-none outline-none font-medium pl-5" rows="" maxlength="380">Write a Comment...</textarea>
+                        <script>
+                            const textArea = document.querySelector('textarea')
+        
+                            textArea.addEventListener("focus", function () {
+                                if (this.value === "What's in your mind?") {
+                                    this.value = "";
+                                }
+                            });
+                            textArea.addEventListener('blur', function () {
+                                if (this.value === '') {
+                                    this.value = "What's in your mind?";
+                                }
+                            });
+                            
+                            </script>
+                            </div>
+                            <!-- Cambia el botón por uno de enviar -->
+                            <button class="text-gray-400 w-10 h-10 transition-all cursor-pointer ml-5 mt-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path class="fill-current" d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3.3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/></svg>
+                            </button>
+                            </div>
+                            </section>
+                            </section>`
+                            
+                            document.body.appendChild($section);
+                            capaComments.classList.remove("hidden");
+                            modalVisible2 = true;
+                            $body.classList.add('overflow-hidden');
+                            window.scrollTo({ top: 0, behavior: 'smooth' });
+                            $section.style.display = 'block';
+
+                            const closeButton = document.getElementById('close-button');
+                            closeButton.addEventListener('click', () => {
+                                console.log("click");
+                                $section.style.display = 'none';
+                                capaComments.classList.add("hidden");
+                                modalVisible2 = false;
+                                $body.classList.remove('overflow-hidden');
+                            })
+
+                        }
+    })
 });
